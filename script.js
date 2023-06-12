1f651d154fefd041aeebc108caa42afc020f0bf2
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-12 12:54:46 -0400

    Added code below the soluton to try to understand the differences

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('test problem 4');
+/*function fizzBuzz(){
+  for(let i = 1; i <= 10; i++){
+    let word = ""; //to reduce conditionals, we can construct the word & check two things instead of three.
+
+    if (i % 3 ===0){                           
+      word += "Fizz";
+    }
+    if (i % 5 ===0){                    
+      word += "Buzz";
+    }
+
+    console.log(i, word);  
+  }
+}
+
+fizzBuzz();
+*/
+
+function fizzBuzz(){
+	for (let i = 1; i <= 10; i ++){
+		if (i % 3 ===0){
+			return "Fizz";
+		}
+		if (i % 5 ===0){
+			return "Buzz";
+		} 
+		console.log(i)
+	}
+	
+}
+
+fizzBuzz();
