bbcee51c6f5217d612e0616a4aa4eb5a1a73602c
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-14 09:36:39 -0400

    Added comments to explain the logic

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let fizzBuzz = () => {
 	for (let i = 1; i < 100; i ++){
 		let multiple = ""
+//provides a place to insert fizz and buzz
+
 		if (i % 3 === 0) {multiple += "Fizz";}
 		if (i % 5 === 0) {multiple += "Buzz";}
+//used the modelo to set the remainder to 0 if divided by 3 or 5
+
 		console.log(i, multiple);
+//prints the increasing index of numbers and the value of the multiple variable
 	}
 		
 }
