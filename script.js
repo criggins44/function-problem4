deb338f677054f137ee2bab1ebe068d55bc7b2bb
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-14 08:50:24 -0400

    Adjusted code after understanding the logic

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
-/*function fizzBuzz(){
-  for(let i = 1; i <= 10; i++){
-    let word = ""; //to reduce conditionals, we can construct the word & check two things instead of three.
-
-    if (i % 3 ===0){                           
-      word += "Fizz";
-    }
-    if (i % 5 ===0){                    
-      word += "Buzz";
-    }
-
-    console.log(i, word);  
-  }
-}
-
-fizzBuzz();
-*/
-
-function fizzBuzz(){
-	for (let i = 1; i <= 10; i ++){
-		if (i % 3 ===0){
-			return "Fizz";
-		}
-		if (i % 5 ===0){
-			return "Buzz";
-		} 
-		console.log(i)
+let fizzBuzz = () => {
+	for (let i = 1; i < 100; i ++){
+		let multiple = ""
+		if (i % 3 === 0) {multiple += "Fizz";}
+		if (i % 5 === 0) {multiple += "Buzz";}
+		console.log(i, multiple);
 	}
-	
+		
 }
 
 fizzBuzz();
